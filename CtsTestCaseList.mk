f556d7da13e9a6332558f99582c3285d61d615e0
Lorenzo Colitti <lorenzo@google.com>
2015-08-11 23:03:53 +0900

    Update permissions tests for legacy ConnectivityManager APIs.
    http://ag/742872 fixed the main ConnectivityManager CTS tests,
    but missed these.
    
    Bug: 23040704
    Change-Id: I67f6292d97c09d9ff90d62dcd0e6fb703ebcc0fb

--- CtsTestCaseList.mk
+++ CtsTestCaseList.mk
@@ -173,6 +173,7 @@ cts_test_packages := \
     CtsNdefTestCases \
     CtsNetTestCases \
     CtsNetTestCasesLegacyApi22 \
+    CtsNetTestCasesLegacyPermission22 \
     CtsOpenGLTestCases \
     CtsOpenGlPerfTestCases \
     CtsOsTestCases \
