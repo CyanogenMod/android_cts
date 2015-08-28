c2b384c3a496a41171ca7c0acac68409fe203fdd
Eric Laurent <elaurent@google.com>
2015-08-28 16:40:30 -0700

    Add CTS tests for IAudioflinger binder checks.
    Bug: 19573085.
    Bug: 20731946.
    Change-Id: Iedaa5e063052ff6ec428438a1437156ae80dd671

--- tests/tests/security/jni/Android.mk
+++ tests/tests/security/jni/Android.mk
@@ -31,11 +31,29 @@ LOCAL_SRC_FILES := \
 		android_security_cts_SELinuxTest.cpp \
 		android_security_cts_MMapExecutableTest.cpp \
 		android_security_cts_AudioPolicyBinderTest.cpp \
-		android_security_cts_EncryptionTest.cpp
+		android_security_cts_EncryptionTest.cpp \
+		android_security_cts_MediaCryptoTest.cpp \
+		android_security_cts_MediaPlayerInfoLeakTest.cpp \
+		android_security_cts_AudioEffectBinderTest.cpp \
+		android_security_cts_AudioFlingerBinderTest.cpp
 
-LOCAL_C_INCLUDES := $(JNI_H_INCLUDE)
+LOCAL_C_INCLUDES := \
+	$(JNI_H_INCLUDE) \
+	system/core/include \
+	frameworks/base/media/jni
 
-LOCAL_SHARED_LIBRARIES := libnativehelper liblog libbinder libutils libmedia libselinux libdl libcutils libcrypto
+LOCAL_SHARED_LIBRARIES := \
+	libnativehelper \
+	liblog \
+	libbinder \
+	libutils \
+	libmedia \
+	libselinux \
+	libdl \
+	libcutils \
+	libcrypto \
+	libstagefright_foundation \
+	libmedia_jni
 
 LOCAL_C_INCLUDES += ndk/sources/cpufeatures
 LOCAL_STATIC_LIBRARIES := cpufeatures
