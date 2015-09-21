be52d802631fc3a87fa5a8a5e22951d27ace7e28
Wei Jia <wjia@google.com>
2015-09-21 10:26:49 -0700

    rename android_security_cts_AudioflingerBinderTest.cpp to android_security_cts_AudioFlingerBinderTest.cpp
    Bug: 19573085.
    Bug: 20731946.
    Change-Id: I9705e3c3bbfc5d95af0708500885ad63b77e17bd
    Ticket: CYNGNOS-1189

--- tests/tests/security/jni/Android.mk
+++ tests/tests/security/jni/Android.mk
@@ -33,7 +33,7 @@ LOCAL_SRC_FILES := \
 		android_security_cts_MMapExecutableTest.cpp \
 		android_security_cts_NetlinkSocket.cpp \
 		android_security_cts_AudioPolicyBinderTest.cpp \
-		android_security_cts_AudioflingerBinderTest.cpp \
+		android_security_cts_AudioFlingerBinderTest.cpp \
 		android_security_cts_MediaPlayerInfoLeakTest.cpp \
 		android_security_cts_AudioEffectBinderTest.cpp
 
