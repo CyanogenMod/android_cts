36919689083bfc94efa7a746e295cf2d83deb28c
Wonsik Kim <wonsik@google.com>
2015-09-30 18:22:30 +0900

    DO NOT MERGE security: Test OOB arbitrary write at AMessage::FromParcel
    Bug: 24123723
    Change-Id: Icf427b0b3ef2e559c38e0a778dfbcbe60e522ef4

--- tests/tests/security/jni/CtsSecurityJniOnLoad.cpp
+++ tests/tests/security/jni/CtsSecurityJniOnLoad.cpp
@@ -26,6 +26,7 @@ extern int register_android_security_cts_AudioPolicyBinderTest(JNIEnv* env);
 extern int register_android_security_cts_AudioFlingerBinderTest(JNIEnv* env);
 extern int register_android_security_cts_MediaPlayerInfoLeakTest(JNIEnv* env);
 extern int register_android_security_cts_AudioEffectBinderTest(JNIEnv* env);
+extern int register_android_security_cts_StagefrightFoundationTest(JNIEnv* env);
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     JNIEnv *env = NULL;
@@ -67,6 +68,10 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     }
 
     if (register_android_security_cts_AudioEffectBinderTest(env)) {
+        return JNI_ERR;
+    }
+
+    if (register_android_security_cts_StagefrightFoundationTest(env)) {
         return JNI_ERR;
     }
 
