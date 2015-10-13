7dc91b35ffed3cdc5ed30522f64c1b95fbdf72a4
Stephen Barber <smbarber@google.com>
2015-10-13 15:06:42 -0700

    Add FileSystemPermissionTest exception for /dev/dri/renderD129.
    Upstream kernels have now split functionality for display controllers
    and GPU rendering into different nodes under /dev/dri. Legacy
    /dev/dri/card* nodes are still available, but so are /dev/dri/controlD* and
    /dev/dri/renderD*. See https://www.kernel.org/doc/htmldocs/drm/drmExternals.html
    
    Some devices may also separate display controller and GPU functionality into
    different cards under /dev/dri. For example, Tegra exposes display controller
    functionality through /dev/dri/{card0,controlD64,renderD128}, and GPU
    functionality through /dev/dri/{card1,controlD65,renderD129}. For apps to
    have access to any GPU functionality, /dev/dri/renderD129 needs to be world-
    writable.
    
    Bug: 23798677
    Change-Id: Ia9eab3927869dfc25cde7e08eafa6c74501bee6b

--- tests/tests/permission/src/android/permission/cts/FileSystemPermissionTest.java
+++ tests/tests/permission/src/android/permission/cts/FileSystemPermissionTest.java
@@ -788,8 +788,10 @@ public class FileSystemPermissionTest extends AndroidTestCase {
                 new File("/dev/binder"),
                 new File("/dev/card0"),       // b/13159510
                 new File("/dev/renderD128"),
+                new File("/dev/renderD129"),  // b/23798677
                 new File("/dev/dri/card0"),   // b/13159510
                 new File("/dev/dri/renderD128"),
+                new File("/dev/dri/renderD129"), // b/23798677
                 new File("/dev/felica"),     // b/11142586
                 new File("/dev/felica_ant"), // b/11142586
                 new File("/dev/felica_cen"), // b/11142586
