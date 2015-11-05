52013e192691aad9277e50793fc02064cc32c3a8
Shreyas Basarge <snb@google.com>
2015-11-05 17:02:27 +0000

    Fix for Idle Mode Constraint test
    This test failed previously because it attempted to
    simulate idle mode by broadcasting an intent while the
    screen is on. This patch asks the user to switch off
    the screen before running the idle mode test.
    
    Bug: 24309062
    Change-Id: I999df8ae2d2564c40612708a5497e3dddc4c6533

--- apps/CtsVerifier/src/com/android/cts/verifier/jobscheduler/IdleConstraintTestActivity.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/jobscheduler/IdleConstraintTestActivity.java
@@ -26,15 +26,17 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  *  Idle constraints:
- *      The framework doesn't support turning idle mode off. Use the manual tester to ensure that
- *      the device is not in idle mode (by turning the screen off and then back on) before running
- *      the tests.
+ *      The framework doesn't support turning the screen off. Use the manual tester to
+ *      turn off the screen to run to run tests that require idle mode to be on.
  */
 @TargetApi(21)
 public class IdleConstraintTestActivity extends ConstraintTestActivity {
@@ -57,22 +59,35 @@ public class IdleConstraintTestActivity extends ConstraintTestActivity {
      */
     private static final int IDLE_ON_JOB_ID = IdleConstraintTestActivity.class.hashCode() + 1;
 
+    private static final int IDLE_ON_TEST_STATE_NOT_IN_PROGRESS = 0;
+    private static final int IDLE_ON_TEST_STATE_WAITING_FOR_SCREEN_OFF = 1;
+
     /**
-     * Listens for idle mode off/on events, namely {@link #ACTION_EXPEDITE_IDLE_MODE} and
-     * {@link Intent#ACTION_SCREEN_ON}.
-     * On ACTION_EXPEDITE_IDLE_MODE, we will disable the {@link #mStartButton}, and on
-     * ACTION_SCREEN_ON we enable it. This is to avoid the start button being clicked when the
-     * device is in idle mode.
+     * mTestState stores the state of the tests. It is used to ensure that we only run
+     * the 'idle on' test if screen is turned off after the user has started tests.
      */
-    private BroadcastReceiver mIdleChangedReceiver = new BroadcastReceiver() {
+    private int mTestState = IDLE_ON_TEST_STATE_NOT_IN_PROGRESS;
+
+    private PowerManager mPowerManager;
+    private TextView mContinueInstructionTextView;
+
+    /**
+     * Listens for screen off event. Starts an async task to force device into
+     * idle mode and run the 'idle on' test.
+     */
+    private BroadcastReceiver mScreenOffReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
-                mStartButton.setEnabled(true);
-            } else if (ACTION_EXPEDITE_IDLE_MODE.equals(intent.getAction())) {
-                mStartButton.setEnabled(false);
+            if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
+                if (mTestState == IDLE_ON_TEST_STATE_WAITING_FOR_SCREEN_OFF) {
+                    mContinueInstructionTextView.setVisibility(View.GONE);
+                    PowerManager.WakeLock wl = mPowerManager.newWakeLock(
+                        PowerManager.PARTIAL_WAKE_LOCK, TAG);
+                    wl.acquire();
+                    new TestIdleModeTaskIdle().execute(wl);
+                }
             } else {
-                Log.e(TAG, "Invalid broadcast received, was expecting SCREEN_ON");
+                Log.e(TAG, "Invalid broadcast received, was expecting SCREEN_OFF");
             }
         }
     };
@@ -86,47 +101,88 @@ public class IdleConstraintTestActivity extends ConstraintTestActivity {
         setPassFailButtonClickListeners();
         setInfoResources(R.string.js_idle_test, R.string.js_idle_instructions, -1);
         mStartButton = (Button) findViewById(R.id.js_idle_start_test_button);
+        mContinueInstructionTextView = (TextView) findViewById(
+            R.id.js_idle_continue_instruction_view);
 
-        // Register receiver for idle off/on events.
+        // Register receiver for screen off event.
         IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
-        intentFilter.addAction(ACTION_EXPEDITE_IDLE_MODE);
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
 
-        registerReceiver(mIdleChangedReceiver, intentFilter);
+        mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+
+        registerReceiver(mScreenOffReceiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Enable start button only if tests are not in progress.
+        if (mTestState == IDLE_ON_TEST_STATE_NOT_IN_PROGRESS) {
+            mStartButton.setEnabled(true);
+            mContinueInstructionTextView.setVisibility(View.GONE);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(mIdleChangedReceiver);
+        unregisterReceiver(mScreenOffReceiver);
     }
 
     @Override
     protected void startTestImpl() {
-        new TestIdleModeTask().execute();
+        mStartButton.setEnabled(false);
+        new TestIdleModeTaskNotIdle().execute();
     }
 
-    /** Background task that will run the actual test. */
-    private class TestIdleModeTask extends AsyncTask<Void, Void, Void> {
-
+    /** Background task that will run the 'not idle' test. */
+    private class TestIdleModeTaskNotIdle extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
             testIdleConstraintFails_notIdle();
+            return null;
+        }
 
+        @Override
+        protected void onPostExecute(Void result) {
+            mTestState = IDLE_ON_TEST_STATE_WAITING_FOR_SCREEN_OFF;
+            mContinueInstructionTextView.setVisibility(View.VISIBLE);
+        }
+    }
 
-            // Send the {@link #ACTION_EXPEDITE_IDLE_MODE} broadcast as an ordered broadcast, this
-            // function will block until all receivers have processed the broadcast.
+    /** Background task that will run the 'idle' test. */
+    private class TestIdleModeTaskIdle extends AsyncTask<PowerManager.WakeLock, Void, Void> {
+
+        private PowerManager.WakeLock mPartialWakeLock;
+
+        @Override
+        protected Void doInBackground(PowerManager.WakeLock... wakeLocks) {
+            mPartialWakeLock = wakeLocks[0];
+
             if (!sendBroadcastAndBlockForResult(new Intent(ACTION_EXPEDITE_IDLE_MODE))) {
-                // Fail the test if the broadcast wasn't processed.
                 runOnUiThread(new IdleTestResultRunner(IDLE_ON_JOB_ID, false));
+            } else {
+                testIdleConstraintExecutes_onIdle();
             }
-
-            testIdleConstraintExecutes_onIdle();
-
             notifyTestCompleted();
             return null;
         }
 
+        @Override
+        protected void onPostExecute(Void result) {
+            // Reset test state
+            mTestState = IDLE_ON_TEST_STATE_NOT_IN_PROGRESS;
+
+            PowerManager.WakeLock fullWakeLock = mPowerManager.newWakeLock(
+                PowerManager.FULL_WAKE_LOCK
+                | PowerManager.ACQUIRE_CAUSES_WAKEUP
+                | PowerManager.ON_AFTER_RELEASE, TAG);
+            // Turn on screen and release both locks
+            fullWakeLock.acquire();
+            fullWakeLock.release();
+            mPartialWakeLock.release();
+        }
     }
 
     /**
@@ -154,6 +210,10 @@ public class IdleConstraintTestActivity extends ConstraintTestActivity {
         runOnUiThread(new IdleTestResultRunner(IDLE_OFF_JOB_ID, testPassed));
     }
 
+    /**
+     * Called after screen is switched off and device is forced into idle mode.
+     * Schedule a job with an idle constraint and verify that it executes.
+     */
     private void testIdleConstraintExecutes_onIdle() {
         mTestEnvironment.setUp();
         mJobScheduler.cancelAll();
@@ -172,6 +232,7 @@ public class IdleConstraintTestActivity extends ConstraintTestActivity {
             // We'll just indicate that it failed, not why.
             testPassed = false;
         }
+
         runOnUiThread(new IdleTestResultRunner(IDLE_ON_JOB_ID, testPassed));
     }
 
