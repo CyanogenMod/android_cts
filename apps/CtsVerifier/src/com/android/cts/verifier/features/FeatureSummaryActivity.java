fc56b9bcc91aa505af4fd3423f1bb88c13241108
Jim Miller <jaggies@google.com>
2015-11-13 16:49:44 -0800

    Add fingerprint feature to cts verifier app
    Fixes bug 25457340
    
    Change-Id: Ib83dbc1f40da0e5df6309608607f272419d042d7

--- apps/CtsVerifier/src/com/android/cts/verifier/features/FeatureSummaryActivity.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/features/FeatureSummaryActivity.java
@@ -233,6 +233,7 @@ public class FeatureSummaryActivity extends PassFailButtons.ListActivity {
     public static final Feature[] ALL_MNC_FEATURES = {
             new Feature(PackageManager.FEATURE_MIDI, false),
             new Feature(PackageManager.FEATURE_AUDIO_PRO, false),
+            new Feature(PackageManager.FEATURE_FINGERPRINT, false),
     };
 
     @Override
