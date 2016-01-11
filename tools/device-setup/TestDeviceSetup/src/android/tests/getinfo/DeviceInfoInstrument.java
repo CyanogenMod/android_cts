558dd2cc0645cd086403e558d65c501d98f7a956
Nicholas Sauer <nicksauer@google.com>
2016-01-11 09:40:09 -0800

    DO NOT MERGE Collect ro.build.reference.fingerprint
    bug: 26187474
    Change-Id: I45acab8cfb31b9b14133bfb1748e80b23d7eb2a3

--- tools/device-setup/TestDeviceSetup/src/android/tests/getinfo/DeviceInfoInstrument.java
+++ tools/device-setup/TestDeviceSetup/src/android/tests/getinfo/DeviceInfoInstrument.java
@@ -81,6 +81,9 @@ public class DeviceInfoInstrument extends Instrumentation implements DeviceInfoC
         addResult(BUILD_ABIS_64, TextUtils.join(",", Build.SUPPORTED_64_BIT_ABIS));
         addResult(SERIAL_NUMBER, Build.SERIAL);
 
+        addResult(REFERENCE_BUILD_FINGERPRINT,
+            SystemProperties.get("ro.build.reference.fingerprint", ""));
+
         addResult(VERSION_RELEASE, Build.VERSION.RELEASE);
         addResult(VERSION_SDK, Build.VERSION.SDK);
         addResult(VERSION_BASE_OS, SystemProperties.get("ro.build.version.base_os", ""));
@@ -459,7 +462,7 @@ public class DeviceInfoInstrument extends Instrumentation implements DeviceInfoC
     private String getTotalMemory() {
         ActivityManager activityManager = (ActivityManager) getContext()
                 .getSystemService(Context.ACTIVITY_SERVICE);
-        
+
         MemoryInfo memoryInfo = new MemoryInfo();
         activityManager.getMemoryInfo(memoryInfo);
         return String.valueOf(memoryInfo.totalMem);
